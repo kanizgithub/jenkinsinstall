812b8f48e485bf421ceca73d079dfcdfd609e519
kanizgithub <104589809+kanizgithub@users.noreply.github.com>
2022-10-25 19:37:49 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,4 +6,5 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update -y
 sudo apt install jenkins -y
-sudo systemctl status jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
