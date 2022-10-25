6cd9b698ad2a0e6fa2d0a941da85a5e33351b571
Ubuntu <ubuntu@ip-172-31-93-221.ec2.internal>
2022-10-25 23:25:21 +0000

    new jenkins file

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#jenkins install
+sudo apt update -y
+sudo apt install openjdk-11-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl status jenkins
